c2caddf3e8c3b1d255f541860e4db223d6a8368b
Sarah <sarah.G2018@outlook.com>
2023-05-21 12:51:17 +0200

    colors too

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors:{
+      white1:"#F3F4F7",
+      white2:"#E2E4E9",
+      white3:"#C7CAD1",
+      black1:"#808080",
+      black2:"#525252",
+      black3:"#3D3D3D",
+      orange:"#F79E1B",
+      orange1:"#FFC100",
+      orange2:"#FF7400",
+      orange3:"#FF4D00",
+
+    },
+    extend: {  
+      fontFamily: {
+      PlusJakartaSans: ["PlusJakartaSans", "sans-serif"],
+      PlayFairDisplay: ["PlayFairDisplay", "serif"],
+    },
   },
+},
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
